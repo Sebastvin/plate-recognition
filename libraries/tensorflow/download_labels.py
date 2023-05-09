6b22206638387a21397a199a2bdb0f0ac69d9614
mzawadzki0 <75886065+mzawadzki0@users.noreply.github.com>
2023-05-09 18:26:19 +0200

    Add a script to save label data locally

---
+++ libraries/tensorflow/download_labels.py
@@ -0,0 +1,19 @@
+import labelbox
+import json
+import secret
+
+
+# Enter your Labelbox API key here
+LB_API_KEY = secret.api_key
+# Create Labelbox client
+lb = labelbox.Client(api_key=LB_API_KEY)
+# Get project by ID
+project = lb.get_project('clh13dzrw0l4a07zmf36u97rd')
+# Export labels created in the selected date range as a json file:
+labels = project.export_labels(download=True, start="2023-04-02", end="2023-05-02")
+
+labels_json = json.dumps(labels)
+
+# Save to a file
+with open('datasets/labels.json', 'w') as out:
+    out.write(labels_json)
